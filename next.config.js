e756daf0cab514b28fe2274d902d6b9eab652159
Davi Herminio <daviherminio@gmail.com>
2023-07-12 11:31:15 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['store.storeimages.cdn-apple.com'],
+  },
+  experimental: {
+    newNextLinkBehavior: false,
+  }
+};
 
 module.exports = nextConfig
