e52938a30a59be14ce5c76fd235f7b7cf8107938
Davi Herminio <daviherminio@gmail.com>
2023-07-13 15:50:20 -0300

    finalizado2345

--- src/components/CartTable.tsx
+++ src/components/CartTable.tsx
@@ -50,7 +50,7 @@ const CartTableRow = (props: {
           size="sm"
           onClick={() => removeProduct(props.entry.product.id)}
         >
-          –
+          
         </Button>
       </td>
     </tr>
